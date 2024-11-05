38f475d9a2caa936c5b273ab97c98512d6281352
Ali <abdulrahman.ali1414@gmail.com>
2024-11-05 22:31:10 +0100

    Still working on ohi 90 and finished comining all the s11 files

---
+++ FilesAppending.py
@@ -0,0 +1,27 @@
+import os
+import pandas as pd
+
+# Directory containing the CSV files
+folder_path = r'E:\Data_V3\Data\S11\Combined'
+
+# Output file where the combined data will be saved
+output_file = 'S11_allData.csv'
+
+# List to hold all the dataframes
+dataframes = []
+
+# Iterate over all the CSV files in the folder
+for filename in os.listdir(folder_path):
+    if filename.endswith(".csv"):  # Process only CSV files
+        file_path = os.path.join(folder_path, filename)
+        # Read each CSV file into a dataframe
+        df = pd.read_csv(file_path)
+        dataframes.append(df)
+
+# Concatenate all dataframes
+combined_df = pd.concat(dataframes, ignore_index=True)
+
+# Save the combined dataframe to a new CSV file
+combined_df.to_csv(output_file, index=False)
+
+print(f"All files have been combined and saved to {output_file}")
--- Phi90.py
+++ Phi90.py
@@ -24,3 +24,35 @@ for i in range(1, 2):
             if match:
                 param_values[param] = float(match.group(1))
     print(param_values)
+    Theta = []
+    Phi = []
+    AbsDir = []
+    with open(file_path, 'r') as file:
+        data_section = None  # Track which section (S2, S3, ...) we're processing
+        for line in file:
+            # Detect start of data sections for S2 to S31
+            if line.strip() == "farfield (f=20)":
+                # Toggle to next section (S2 to S31) after encountering each separator
+                if data_section is None:
+                    data_section = 'Theta'
+                elif data_section == 'Theta':
+                    data_section = 'Phi'
+                elif data_section == 'Phi':
+                    data_section = 'Abs(Dir.)'
+                continue
+
+            parts = line.strip().split()
+            if len(parts) == 2:  # Ensure line has exactly two elements
+                freq = float(parts[0])  # Frequency in GHz
+                if data_section == 'Theta':
+                    s2_mag = float(parts[1])  # S2 magnitude in dB
+                    frequencies.append(freq)
+                    Theta.append(s2_mag)
+                elif data_section == 'Phi':
+                    s3_mag = float(parts[1])  # S3 magnitude in dB
+                    Phi.append(s3_mag)
+                elif data_section == 'Abs':
+                    s4_mag = float(parts[1])  # S4 magnitude in dB
+                    AbsDir.append(s4_mag)
+    df9 = pd.DataFrame({'S9 Magnitude (dB)': AbsDir})
+    print(df9)
